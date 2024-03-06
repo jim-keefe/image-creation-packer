9ea0a73773723296ebde80c4475b1b8617278944
jim-keefe <jimkeefe@icloud.com>
2024-03-06 01:03:09 -0600

    Add Powershell worker scripts for Jenkins

--- provisionhv-vm.ps1
+++ provisionhv-vm.ps1
@@ -15,6 +15,21 @@ $myscriptpathparent = (get-item $myscriptpath).Directory
 . "$myscriptpathparent\provisionhv-functions.ps1"
 
 #======================================
+Header -message "Read in State JSON" -level 2
+#======================================
+
+$basepath = "E:\Hyper-V"
+if ($env:BUILD_TAG){
+    $jsonPath = "$basepath\Management\pipelineJSON\$($env:BUILD_TAG).json"
+} else {
+    $jsonPath = "$basepath\Management\pipelineJSON\$OSversion$("-")$OSflavor.json"   
+}
+
+if ( Test-path -path $jsonPath ) { $oState = [pscustomobject](Get-Content -Path $jsonPath | ConvertFrom-Json) }
+
+"vmname : $($env:BUILD_TAG)"
+"vmname : $($oState.vmname)"
+#======================================
 Header -message "Create Folder Structure" -level 2
 #======================================
 
@@ -47,7 +62,17 @@ if ($createVM){ New-VM @vmCreate }
 #======================================
 Header -message "Set the number of processors" -level 2
 #======================================
-$createVM
+
 if ($createVM){ Set-VMProcessor -VMName $VMName -Count $ProcessorCount }
 
 Start-VM -Name $VMName
+
+#======================================
+Header -message "Save State to JSON" -level 2
+#======================================
+
+
+Add-Member -InputObject $oState -Name "vmname" -Value $VMName -MemberType NoteProperty
+Add-Member -InputObject $oState -Name "vhdxtemplatepath" -Value $vhdxTemplatePath -MemberType NoteProperty
+
+set-content -Value $(ConvertTo-Json -InputObject $oState) -Path $jsonPath
